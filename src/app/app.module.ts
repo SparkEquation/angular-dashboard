50d2da42dade260c9aa383132afd1550df970878
Igor Kulebyakin <igk19@rambler.ru>
2020-02-16 23:03:31 +0300

    First commit with HMR enabled.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgxsModule, StateContext } from "@ngxs/store";
+import {
+    NgxsHmrLifeCycle,
+    NgxsHmrSnapshot as Snapshot
+} from "@ngxs/hmr-plugin";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, NgxsModule.forRoot([])],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements NgxsHmrLifeCycle<Snapshot> {
+    public hmrNgxsStoreOnInit(
+        ctx: StateContext<Snapshot>,
+        snapshot: Partial<Snapshot>
+    ) {
+        ctx.patchState(snapshot);
+    }
+
+    public hmrNgxsStoreBeforeOnDestroy(
+        ctx: StateContext<Snapshot>
+    ): Partial<Snapshot> {
+        return ctx.getState();
+    }
+}
